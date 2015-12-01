e409ce068368d59b331a9e7767c58b20c7894b8b
Eugene DiBenedetto <eldibenedetto@gmail.com>
2015-12-01 14:23:02 -0500

    test

--- lib/ruby_basics.rb
+++ lib/ruby_basics.rb
@@ -1,27 +1,27 @@
 def division(num1, num2)
- 
+    num1 / num2
 end
 
-def assign_variable(value)
-  
+def assign_variable(name)
+  first_name = name
 end
 
-def argue
-  
+def argue(side)
+  side
 end
 
-def greeting
+def greeting(name, me)
 
 end
 
 def return_a_value
-
+    "Nice"
 end
 
 def last_evaluated_value
-
+    "expert"
 end
 
-def pizza_party
-
+def pizza_party(topping = "cheese")
+    topping
 end
